20620a4a1280f82c2b258ff485074bc60205a48b
Heitor Rangel <heitor.roranm@gmail.com>
2025-08-03 16:16:28 -0300

    Adiciona Banco de Dados

--- models.py
+++ models.py
@@ -27,7 +27,46 @@ class Usuario(Base):
 
 
 # Pedido
+class Pedido(Base):
+    __tablename__ = "pedidos"
+    
+    # STATUS_PEDIDOS = (
+    #     ("PENDENTE", "PENDENTE"),
+    #     ("CANCELADO", "CANCELADO"),
+    #     ("FINALIZADO", "FINALIZADO")
+    # )
+    
+    id = Column("id", Integer, primary_key=True, autoincrement=True)
+    usuario = Column("usuario", ForeignKey("usuarios.id"))
+    status = Column("status", String)
+    preco = Column("preco", Float)
+    # itens =
+    
+    
+    def __init__(self, usuario, status="PENDENTE", preco=0):
+        self.usuario = usuario
+        self.status = status
+        self.preco = preco
+
 
 # Itens do pedido
+class ItemPedido(Base):
+    __tablename__ = "itens_pedido"
+    
+    id = Column("id", Integer, primary_key=True, autoincrement=True)
+    quantidade = Column("quantidade", Integer)
+    sabor = Column("sabor", String)
+    tamanho = Column("tamanho", String)
+    preco_unitario = Column("preco_unitario", Float)
+    pedido = Column("pedido", ForeignKey("pedidos.id"))
+    
+    
+    def __init__(self, quantidade, sabor, tamanho, preco_unitario, pedido):
+        self.quantidade = quantidade
+        self.sabor = sabor
+        self.tamanho = tamanho
+        self.preco_unitario = preco_unitario
+        self.pedido = pedido
+
 
 # Executa a ação dos metadados do banco de dados (Cria efetivamente o banco de dados)
